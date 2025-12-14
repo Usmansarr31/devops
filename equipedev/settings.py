1f891eb70a5ad836e92c542e15892309408b10a7
latyrsarr31 <usmansarr31@gmail.com>
2025-12-14 11:52:25 +0000

    Correction du fichier seetings.py et du gichier yml

--- equipedev/settings.py
+++ equipedev/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': 'devops_demo',
         'USER': 'root',
         'PASSWORD': 'Mpi2945@',
-        'HOST': 'localhost',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
         'Options': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
