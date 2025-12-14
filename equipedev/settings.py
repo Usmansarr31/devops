5844030c3eda95db224192eb8f2296aee84deb15
latyrsarr31 <usmansarr31@gmail.com>
2025-12-14 12:17:26 +0000

    Correction du fichier seetings.py et du gichier yml

--- equipedev/settings.py
+++ equipedev/settings.py
@@ -20,10 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-!!u*-ft6=gvj$ndl&g7k)qq33b#jk-&y1kg2e^t)fe%v6+4(@o'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
